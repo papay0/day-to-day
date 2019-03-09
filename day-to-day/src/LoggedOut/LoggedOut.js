1fff2d22883d1388e441603c76d4e12352a12660
Arthur Papailhau <papailhau@uber.com>
2019-03-09 17:07:53 +0100

    Updates design logout page

--- day-to-day/src/LoggedOut/LoggedOut.js
+++ day-to-day/src/LoggedOut/LoggedOut.js
@@ -1,20 +1,76 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-export default class LoggedOut extends Component {
-  render() {
-    const { signInWithGoogle } = this.props;
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
 
-    return (
-      // <div style={{ textAlign: "center", verticalAlign: "middle" }}>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+function LoggedOut(props) {
+  const { classes, signInWithGoogle } = props;
+  // style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
         <Button
-          variant="outlined"
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
           onClick={signInWithGoogle}
         >
-          Sign in with Google
+          With Google
         </Button>
-      </div>
-    );
-  }
+      </Paper>
+    </main>
+  );
 }
+
+LoggedOut.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(LoggedOut);
